83cbe432f340d837842bef69fe63f3f902c98f32
Illgner Alain <illgneralain@gmail.com>
2021-01-26 17:31:18 -0500

    getRole and getGithub (may not have done correctly) inserted and finalized

--- 02-Homework/tests/engineer.test.js
+++ 02-Homework/tests/engineer.test.js
@@ -1,7 +1,7 @@
 const engineer = require("../lib/engineer");
 
 test("set Github account using constructor", () => {
-    const testValue = "GitHubUser";
+    const testValue = "githubUser";
     const newbie = new engineer("Woo", 1, "test@test.com")
     expect(typeof(newbie)).toBe("object");
 });
@@ -10,5 +10,11 @@ test("using getRole()", () => {
     const testValue = "Engineer";
     const newbie = new engineer("Woo", 1, "test@test.com", "GitHubUser");
     expect(newbie.getRole()).toBe(testValue);
+});
+
+test("get github username", () => {
+    const testValue = "githubUser";
+    const newbie = new engineer("Woo", 1, "test@test.com", testValue);
+    expect(newbie.getGithub()).toBe(testValue);
 });
 
