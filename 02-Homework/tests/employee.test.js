0e7e63dd65233cf79c425601956c1dbde1268def
Illgner Alain <illgneralain@gmail.com>
2021-01-26 16:54:05 -0500

    made some changes to the next tests

--- 02-Homework/tests/employee.test.js
+++ 02-Homework/tests/employee.test.js
@@ -17,3 +17,32 @@ test("set id using constructor"), () => {
     expect(newbie.id).toBe(testValue);
 }
 
+test("set email using constructor"), () => {
+    const testValue = "test@test.com";
+    const newbie = new employee("Woo", 1, testValue);
+    expect(newbie.id).toBe(testValue);
+}
+
+test("get name using constructor"), () => {
+    const testValue = "Alice";
+    const newbie = new employee(testValue);
+    expect(newbie.getName).toBe(testValue);
+}
+
+test("set id using constructor"), () => {
+    const testValue = 100;
+    const newbie = new employee("Woo", testValue);
+    expect(newbie.id).toBe(testValue);
+}
+
+test("set id using constructor"), () => {
+    const testValue = 100;
+    const newbie = new employee("Woo", testValue);
+    expect(newbie.id).toBe(testValue);
+}
+
+test("set id using constructor"), () => {
+    const testValue = 100;
+    const newbie = new employee("Woo", testValue);
+    expect(newbie.id).toBe(testValue);
+}
